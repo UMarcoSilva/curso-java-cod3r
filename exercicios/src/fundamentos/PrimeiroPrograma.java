131b7dcc02435a8f311b40c710cde346cb0681a8
UMarcoSilva <marcoadias.silva@gmail.com>
2021-10-12 17:13:31 -0300

    Update PrimeiroPrograma.java

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,5 +1,30 @@
 package fundamentos;
+/**
+ * Essa classe representa... JAVADOC
+ * @author marco Antonio <marcoadias.silva@gmail.com)
+ * @since jdk1.0
+ * @see
+ */
 
 public class PrimeiroPrograma {
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+
+		// uma sentença de codigo ;
+		// mais um comentario...
+		// fim
+		System.out.println("Primeiro programa Parte #1!!");
+
+		/*
+		 * 
+		 * linha 1 linha 2 linha 3...
+		 */
+		
+		System.out.println("fim"); //aq tbem funciona
+	}
 
 }

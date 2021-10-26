fdd2c02d49b0b1dd8bf721e7ed49b8dfb0a58094
UMarcoSilva <marcoadias.silva@gmail.com>
2021-10-26 17:10:47 -0300

    .

---
+++ Exercicios/src/fundamentos/operadores/Ternario.java
@@ -0,0 +1,20 @@
+package fundamentos.operadores;
+
+public class Ternario {
+	public static void main(String[] args) {
+
+		double media = 7.6;
+		String resultadoFinal = media >= 7.00 ? 
+				"Aprovado" : "Recuperação";	
+		System.out.println("O aluno está " + resultadoFinal);
+		
+		double nota = 7.3;
+		boolean bomComportamento = true;
+		boolean passouPorMedia = nota >= 7;
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		String resultado = temDesconto ? "sim" : "não";
+		
+		System.out.printf("Tem deconto? %s", resultado);
+		
+	}
+}
